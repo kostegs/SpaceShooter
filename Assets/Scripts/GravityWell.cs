8ba4ec30e293641d45fce15620328163728a14a5
Kostegs <ksuslin@gmail.com>
2023-06-15 08:55:43 +0300

    Added GravityWell script

---
+++ Assets/Scripts/GravityWell.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class GravityWell : MonoBehaviour
+    {
+        [SerializeField] private float _force;
+        [SerializeField] private float _radius;
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (collision.attachedRigidbody == null)
+                return;
+
+            Vector2 direction = transform.position - collision.transform.position;
+
+            float distance = direction.magnitude;
+            
+            Debug.Log(distance / _radius);
+
+            Vector2 force = direction.normalized * _force * (distance / _radius);
+            collision.attachedRigidbody.AddForce(force, ForceMode2D.Force);
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate() => GetComponent<CircleCollider2D>().radius = _radius;
+#endif
+    }
+}
