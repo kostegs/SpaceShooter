7f5517f0d3d2289a3a61a93048f53e6f1c1953c7
Kostegs <ksuslin@gmail.com>
2023-06-20 09:46:10 +0300

    Modified Distance-Arrow to UFO

--- Assets/Scripts/UI/UIArrow.cs
+++ Assets/Scripts/UI/UIArrow.cs
@@ -1,83 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
-using UnityEngine.UI;
 
 public class UIArrow : MonoBehaviour
 {
-    /*[SerializeField] private Transform _spaceShip;
-    [SerializeField] private Transform _UFO;
-
-    private bool _rotationStarted;
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (!_rotationStarted)            
-            StartCoroutine(Rotation());
-    }
-
-    void Update()
-    {
-        Debug.DrawLine(Vector2.zero, _spaceShip.up * 5, Color.red);
-        Debug.DrawLine(Vector2.zero, (_UFO.position - _spaceShip.position), Color.blue);
-    }
-
-    IEnumerator Rotation()
-    {
-        _rotationStarted = true;
-        
-        float targetAngle = Vector2.SignedAngle(_spaceShip.up, (_UFO.position - _spaceShip.position));
-        float saveTargetAngle = targetAngle;
-
-        //targetAngle += _spaceShip.eulerAngles.z;
-        
-        Debug.Log($"Было: {saveTargetAngle} стало: {targetAngle}");
-
-        float currentVelocity = 0, angle = transform.eulerAngles.z;
-
-        while (Mathf.Floor(angle) != Mathf.Floor(targetAngle))
-        {
-            Debug.Log($"angle: {angle}, targetangle {targetAngle}");    
-            angle = Mathf.SmoothDampAngle(angle, targetAngle, ref currentVelocity, 0.3f, 90);
-            transform.eulerAngles = new Vector3(0, 0, angle);
-            yield return null;
-        }
-
-        transform.eulerAngles = new Vector3(0, 0, targetAngle);
-        _rotationStarted = false;
-    }*/
-
     [SerializeField] private Transform _spaceShip;
     [SerializeField] private Transform _UFO;
     [SerializeField] private Camera _camera;
+    [SerializeField] private TMP_Text _distanceText;
 
-    private void Update()
+    private Vector3 _leftRotation, _rightRotation;
+    private Vector3 _downRotation, _upRotation;
+
+    private Vector2 _fromPlayerToEnemy;
+
+    private void Start()
     {
-        Vector2 fromPlayerToEnemy = _UFO.position - _spaceShip.position;
-        Ray ray = new Ray(_spaceShip.position, fromPlayerToEnemy);
-        Debug.DrawLine(_spaceShip.position, fromPlayerToEnemy);
+        _leftRotation = new Vector3(0f, 0f, 90f);
+        _rightRotation = new Vector3(0f, 0f, -90f);
+        _downRotation = new Vector3(0f, 0f, 180f);
+        _upRotation = Vector3.zero;        
+    }
 
+    private void FixedUpdate()
+    {
+        _fromPlayerToEnemy = _UFO.position - _spaceShip.position;
+
+        _distanceText.text = ((int)_fromPlayerToEnemy.magnitude).ToString();
+
+        Ray ray = new Ray(_spaceShip.position, _fromPlayerToEnemy);
+        
+        // [0] = Left; [1] = Right; [2] = Down; [3] = Up
         Plane[] planes = GeometryUtility.CalculateFrustumPlanes(_camera);
 
         float minDistance = Mathf.Infinity;
+        int planeIndex = 0;
 
         for (int i = 0; i < 4; i++)
         {
             if (planes[i].Raycast(ray, out float distance))
             {
                 if (distance < minDistance)
+                {
                     minDistance = distance;
+                    planeIndex = i;
+                }                    
             }
         }
 
-        minDistance = Mathf.Clamp(minDistance, 0, fromPlayerToEnemy.magnitude);
-        Vector2 worldPosition = ray.GetPoint(minDistance - 0.5f);
+        minDistance = Mathf.Clamp(minDistance, 0, _fromPlayerToEnemy.magnitude);
+        Vector2 worldPosition = ray.GetPoint(minDistance - 1.5f);
 
         transform.position = _camera.WorldToScreenPoint(worldPosition);
+
+        Vector3 eulerRotation = GetEulerRotation(planeIndex);
+
+        transform.rotation = Quaternion.Euler(eulerRotation);
+        _distanceText.transform.rotation = Quaternion.Euler(eulerRotation * 4);
     }
-
-
+    
+    Vector3 GetEulerRotation(int planeIndex)
+    {
+        switch (planeIndex)
+        {
+            case 0:
+                return _leftRotation;
+            case 1:
+                return _rightRotation;
+            case 2:
+                return _downRotation;
+            case 3:
+                return _upRotation;
+            default:
+                return Vector3.zero;
+        }
+    }
 }
