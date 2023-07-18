4c4c72dd4e7c64fd080d21f71607e27cca6d661f
Kostegs <ksuslin@gmail.com>
2023-07-18 10:01:49 +0300

    Added autogoal rocket (standart behaviour), sprite, trail

---
+++ Assets/Scripts/Items/AutoGoalRocket.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class AutoGoalRocket : Projectile
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
