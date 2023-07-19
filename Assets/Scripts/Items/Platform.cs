5cdc0c84878e198de6b1835553efdca6758737e7
Kostegs <ksuslin@gmail.com>
2023-07-19 09:04:37 +0300

    Fixed bug of platform - it showed the sign (press E...) when asteroid made collision with collider

--- Assets/Scripts/Items/Platform.cs
+++ Assets/Scripts/Items/Platform.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 namespace SpaceShooter
 {
-
     public class Platform : MonoBehaviour
     {
         [SerializeField] private GameObject _uiTextPressButton;
@@ -12,13 +11,17 @@ namespace SpaceShooter
 
         private bool _animationStarted;
 
-        private void OnTriggerEnter2D(Collider2D other) => _uiTextPressButton.SetActive(true);
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.GetComponentInParent<SpaceShip>() != null)
+                _uiTextPressButton.SetActive(true);
+        } 
 
         private void OnTriggerExit2D() => _uiTextPressButton.SetActive(false);
 
         private void OnTriggerStay2D(Collider2D collision)
         {
-            if (_animationStarted == false && Input.GetKey(KeyCode.E))
+            if (_animationStarted == false && collision.GetComponentInParent<SpaceShip>() != null && Input.GetKey(KeyCode.E))
             {
                 _animationStarted = true;
                 _uiTextPressButton.SetActive(false);
