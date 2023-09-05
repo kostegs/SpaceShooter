ad7f401c05969db2f2b89f8d4d6af03794b98be4
Kostegs <ksuslin@gmail.com>
2023-09-05 09:50:44 +0300

    Added selection ships in Menu, default ship for LevelSequenceController

--- Assets/Scripts/Service/MainMenuController.cs
+++ Assets/Scripts/Service/MainMenuController.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 namespace SpaceShooter
 {
-    public class MainMenuController : MonoBehaviour
+    public class MainMenuController : MonoSingleton<MainMenuController>
     {
+        [SerializeField] private GameObject _defaultSpaceShip;
         [SerializeField] private GameObject _episodeSelection;
+        [SerializeField] private GameObject _shipSelectionPanel;
+
+        private void Start() => LevelSequenceController.PlayerShipPrefab = _defaultSpaceShip;
 
         public void OnButtonStartNew()
         {
             _episodeSelection.SetActive(true);
             gameObject.SetActive(false);
         }
+
+        public void OnButtonSelectSpaceShip()
+        {
+            _shipSelectionPanel.SetActive(true);
+            gameObject.SetActive(false);
+        }  
 
         public void OnButtonExit() => Application.Quit();
     }
